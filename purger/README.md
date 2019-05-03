c9b2cfd3a6421d484a6dbd068eb8d8e7de3953ca
unknown <robin.mahieu@protonmail.com>
2019-05-03 19:10:18 +0200

    Added purger plugin

---
+++ purger/README.md
@@ -0,0 +1,17 @@
+# Purger
+
+This plugin makes it possible for your moderators to delete multiple messages at a time.
+
+## Installation
+
+To use this plugin, use this command in your Modmail server: `plugin add papiersnipper/modmail-plugins/purger`
+
+## Commands
+
+| name    | aliases  | usage          | example    | description                               |
+|---------|----------|----------------|------------|-------------------------------------------|
+| purge   | delete   | purge amount   | purge 10   | Deletes the provided amount of messages.  |
+
+## Permissions
+
+The bot needs the `manage messages` permission to delete messages.
---
+++ purger/purger.py
@@ -0,0 +1,44 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+
+class Purger:
+    """The purger plugin for Modmail: https://github.com/papiersnipper/modmail-plugins/blob/master/purger"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['delete'])
+    async def purge(self, ctx, amount: str = None):
+        """Deletes the specified amount of messages."""
+
+        owners = self.bot.config.get('owners').split(',')
+
+        if ctx.message.author.guild_permissions.manage_messages or ctx.message.author.id in (int(x) for x in owners):
+            try:
+                if amount is None:
+                    return await ctx.send('Please specify amount of messages.')
+                else:
+                    try:
+                        amount = int(amount)
+                    except Exception:
+                        return await ctx.send('That\'s not a valid amount of messages to delete.')
+                    
+                    if amount > 1 and amount < 100:
+                        await ctx.channel.purge(limit=amount + 1)
+                        delete_message = await ctx.send(f'I successfully deleted {amount} messages!')
+
+                        await asyncio.sleep(3)
+                        await delete_message.delete()
+                    else:
+                        return await ctx.send('The number of messages has been be between 1 and 100.')
+
+            except discord.Forbidden:
+                return await ctx.send('I don\'t have the proper permissions to delete messages.')
+        else:
+            return await ctx.send('You don\'t have the proper permissions to delete messages.')
+
+
+def setup(bot):
+    bot.add_cog(Purger(bot))

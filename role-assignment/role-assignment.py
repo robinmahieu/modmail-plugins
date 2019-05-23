b2f112a5854c21b084c00ec7ef20beeaa0a12e6f
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-05-23 22:30:16 +0530

    Store ID's in the database

--- role-assignment/role-assignment.py
+++ role-assignment/role-assignment.py
@@ -1,5 +1,7 @@
 import asyncio
 import discord
+import os
+from datetime import datetime
 from discord.ext import commands
 
 from core import checks
@@ -14,9 +16,24 @@ class RoleAssignment(Cog):
     """
 
     def __init__(self, bot):
-        self.bot = bot
+        self.bot: discord.Client = bot
         self.db = bot.plugin_db.get_partition(self)
-        self.message_ids = []
+        self.ids = dict()
+
+    async def update_db(self):
+        await self.db.find_one_and_update(
+            {"_id": "role-config"},
+            {"$set": {"ids": self.ids}},
+            upsert=True
+        )
+
+    async def _set_db(self):
+        config = await self.db.find_one({'_id': 'role-config'})
+
+        if config is None:
+            return
+
+        self.ids = dict(config.get("ids", {}))
     
     @commands.group(name='role', aliases=['roles'], invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
@@ -85,16 +102,17 @@ class RoleAssignment(Cog):
         for k, v in (await self.db.find_one({'_id': 'role-config'}))['emoji'].items():
             await message.add_reaction(k)
 
-        self.message_ids.append(message.id)
+        self.ids[str(message.id)] = str(message.channel.id)
+        await self.update_db()
 
     @Cog.listener()
-    async def on_raw_reaction_add(self, payload):
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
 
-        if payload.message_id not in self.message_ids:
+        if str(payload.message_id) not in self.ids:
             return
 
         guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
+        guild: discord.Guild = discord.utils.find(lambda g : g.id == guild_id, self.bot.guilds)
 
         if payload.user_id == self.bot.user.id:
             return
@@ -117,11 +135,11 @@ class RoleAssignment(Cog):
 
         await member.add_roles(role)
         await guild.get_channel(payload.channel_id).send(f'Successfully added {role} to {member.name}')
-    
+
     @Cog.listener()
     async def on_raw_reaction_remove(self, payload):
 
-        if payload.message_id not in self.message_ids:
+        if str(payload.message_id) not in self.ids:
             return
 
         guild_id = payload.guild_id
@@ -146,6 +164,39 @@ class RoleAssignment(Cog):
         await member.remove_roles(role)
         await guild.get_channel(payload.channel_id).send(f'Successfully removed {role} from {member.name}')
 
+    @Cog.listener()
+    async def on_guild_channel_delete(self,channel):
+        if isinstance(channel, discord.TextChannel):
+            if channel.category_id != self.bot.config.get('main_category_id'):
+                return
+
+            for msg_id, channel_id in self.ids.items():
+                if channel_id == str(channel.id):
+                    self.ids.pop(msg_id)
+                    await self.update_db()
+
+
+
+    @Cog.listener()
+    async def on_plugin_ready(self):
+        asyncio.create_task(self._set_db())
+        await asyncio.sleep(30)
+        guild: discord.Guild = self.bot.get_guild(int(os.getenv("GUILD_ID")))
+
+        category: discord.CategoryChannel = guild.get_channel(int(self.bot.config.get('main_category_id')))
+
+        # if category is None:
+        #     return
+
+        for c in category.channels:
+            if isinstance(c,discord.TextChannel):
+                if c.topic and "User ID: " not in c.topic:
+                    return
+                messages = await c.history(oldest_first=True).flatten()
+                if str(messages[0].id) not in self.ids:
+                    self.ids[str(messages[0].id)] = str(c.id)
+
+        await self._update_db()
 
 def setup(bot):
     bot.add_cog(RoleAssignment(bot))

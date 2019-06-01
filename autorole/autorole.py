a190cfdcb99a946486c084398ef9dbcb290869ec
papiersnipper <robin.mahieu@protonmail.com>
2019-06-01 11:56:08 +0200

    Enforce black coding style (autorole)

--- autorole/autorole.py
+++ autorole/autorole.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
 
-Cog = getattr(commands, 'Cog', object)
+Cog = getattr(commands, "Cog", object)
 
 
 class Autorole(Cog):
@@ -18,7 +18,7 @@ class Autorole(Cog):
 
     @Cog.listener()
     async def on_member_join(self, member):
-        rolename = (await self.db.find_one({'_id': 'autorole-config'}))['rolename']
+        rolename = (await self.db.find_one({"_id": "autorole-config"}))["rolename"]
 
         if rolename is None:
             return
@@ -26,30 +26,32 @@ class Autorole(Cog):
             role = discord.utils.get(member.guild.roles, name=rolename)
             await member.add_roles(role)
 
-    @commands.group(name='autorole', invoke_without_command=True)
+    @commands.group(name="autorole", invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def autorole(self, ctx):
         """Auto-assign a role to a user when they join your server."""
 
         await ctx.send_help(ctx.command)
 
-    @autorole.command(name='set')
+    @autorole.command(name="set")
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def set_(self, ctx, role: discord.Role):
         """Sets the default role a member gets when joining."""
 
-        await self.db.find_one_and_update({'_id': 'autorole-config'}, {'$set': {'rolename': role.name}})
+        await self.db.find_one_and_update(
+            {"_id": "autorole-config"}, {"$set": {"rolename": role.name}}
+        )
 
         em = discord.Embed(
-            title='Autorole',
-            url='https://github.com/papiersnipper/modmail-plugins/blob/master/autorole',
-            description=f'I will now give {role.mention} to all new members.',
-            colour=self.bot.main_color
+            title="Autorole",
+            url="https://github.com/papiersnipper/modmail-plugins/blob/master/autorole",
+            description=f"I will now give {role.mention} to all new members.",
+            colour=self.bot.main_color,
         )
 
         await ctx.send(embed=em)
 
-    @autorole.command(name='give')
+    @autorole.command(name="give")
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def give(self, ctx, role: discord.Role):
         """Gives this role to all members of your server."""
@@ -63,10 +65,10 @@ class Autorole(Cog):
                 users = users + 1
 
         em = discord.Embed(
-            title='Autorole',
-            url='https://github.com/papiersnipper/modmail-plugins/blob/master/autorole',
-            description=f'Added {role.mention} for {users} members.',
-            colour=self.bot.main_color
+            title="Autorole",
+            url="https://github.com/papiersnipper/modmail-plugins/blob/master/autorole",
+            description=f"Added {role.mention} for {users} members.",
+            colour=self.bot.main_color,
         )
 
         await ctx.send(embed=em)

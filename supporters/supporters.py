5cd6643c6424557253b2d6d67238bbc1bdd6c3ba
papiersnipper <robin.mahieu@protonmail.com>
2019-06-01 16:07:31 +0200

    Enforce black coding style (supporters)

--- supporters/supporters.py
+++ supporters/supporters.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
 
-Cog = getattr(commands, 'Cog', object)
+Cog = getattr(commands, "Cog", object)
 
 
 class Supporters(Cog):
@@ -15,39 +15,44 @@ class Supporters(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['helpers', 'supporters', 'supportmembers'])
+    @commands.command(aliases=["helpers", "supporters", "supportmembers"])
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def support(self, ctx):
         """Send an embed with all the support members."""
 
-        category_id = self.bot.config.get('main_category_id')
+        category_id = self.bot.config["main_category_id"]
 
         if category_id is None:
-            return await ctx.send('I couldn\'t find the modmail category.')
+            embed = discord.Embed(
+                title="Supporters",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/supporters",
+                description=f"I couldn't find the modmail category.\nMake sure it's set using the `?config set main_category_id` command.",
+                color=self.bot.main_color,
+            )
+
+            return await ctx.send(embed=embed)
 
         categories = ctx.guild.categories
 
-        for c in categories:
-            if c.id != int(category_id):
+        for category in categories:
+            if category.id != int(category_id):
                 continue
             else:
-                category = c
-
-        member_list = []
-
-        for member in ctx.guild.members:
-            if member.permissions_in(category).read_messages:
-                if not member.bot:
-                    member_list.append(member.mention)
-                else:
-                    continue
-            else:
-                continue
+                member_list = []
+                for member in ctx.guild.members:
+                    if member.permissions_in(category).read_messages:
+                        if not member.bot:
+                            member_list.append(member.mention)
+                        else:
+                            continue
+                    else:
+                        continue
 
         embed = discord.Embed(
-            title='Support Members',
-            colour=discord.Colour.blue(),
-            description=', '.join(member_list)
+            title="Support Members",
+            url="https://github.com/papiersnipper/modmail-plugins/blob/master/supporters",
+            colour=self.bot.main_color,
+            description=", ".join(member_list),
         )
 
         await ctx.send(embed=embed)

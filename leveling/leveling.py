0dad5a25e6cc576d72a58d58e39c55b71333f399
papiersnipper <robin.mahieu@protonmail.com>
2019-06-01 15:49:27 +0200

    Cleanup code for leveling

--- leveling/leveling.py
+++ leveling/leveling.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
 
-Cog = getattr(commands, 'Cog', object)
+Cog = getattr(commands, "Cog", object)
 
 
 class Leveling(Cog):
@@ -21,53 +21,67 @@ class Leveling(Cog):
         if message.author.bot:
             return
 
-        amount = await self.db.find_one({'_id': 'leveling-config'})
+        config = await self.db.find_one({"_id": "leveling-config"})
 
-        if amount is None:
+        if config is None:
             return
-        else:
-            amount = amount['amount_per_message']
 
-        person = await self.db.find_one({'id': message.author.id})
+        amount = config["amount_per_message"]
+
+        person = await self.db.find_one({"id": message.author.id})
 
         if person is None:
-            await self.db.insert_one({
-                'id': message.author.id,
-                'name': message.author.name,
-                'gold': amount,
-                'exp': amount,
-                'level': 1
-            })
+            await self.db.insert_one(
+                {
+                    "id": message.author.id,
+                    "name": message.author.name,
+                    "gold": amount,
+                    "exp": amount,
+                    "level": 1,
+                }
+            )
         else:
-            new_gold = person['gold'] + amount
-            new_exp = person['exp'] + amount
-            level = int(new_exp ** (1/4))
+            new_gold = person["gold"] + amount
+            new_exp = person["exp"] + amount
+            level = int(new_exp ** (1 / 4))
 
-            if person['level'] < level:
-                await message.channel.send(f'Congratulations, {message.author.mention}, '
-                                           f'you advanced to level {level}!')
+            if person["level"] < level:
+                await message.channel.send(
+                    f"Congratulations, {message.author.mention}, "
+                    f"you advanced to level {level}!"
+                )
 
-                await self.db.update_one({'id': message.author.id},
-                                         {'$set': {'gold': new_gold,
-                                                   'exp': new_exp,
-                                                   'level': level,
-                                                   'name': message.author.name
-                                                   }})
+                await self.db.update_one(
+                    {"id": message.author.id},
+                    {
+                        "$set": {
+                            "gold": new_gold,
+                            "exp": new_exp,
+                            "level": level,
+                            "name": message.author.name,
+                        }
+                    },
+                )
             else:
-                await self.db.update_one({'id': message.author.id},
-                                         {'$set': {'gold': new_gold,
-                                                   'exp': new_exp,
-                                                   'name': message.author.name
-                                                   }})
+                await self.db.update_one(
+                    {"id": message.author.id},
+                    {
+                        "$set": {
+                            "gold": new_gold,
+                            "exp": new_exp,
+                            "name": message.author.name,
+                        }
+                    },
+                )
 
-    @commands.group(name='level', invoke_without_command=True)
+    @commands.group(name="level", invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def level(self, ctx):
         """A leveling system for your server: see who's active and who's not."""
 
         await ctx.send_help(ctx.command)
 
-    @level.command(name='info')
+    @level.command(name="info")
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def info(self, ctx, user: discord.User = None):
         """Check someone's current amount of gold, exp and level."""
@@ -75,87 +89,158 @@ class Leveling(Cog):
         if user is None:
             user = ctx.author
 
-        stats = await self.db.find_one({'id': user.id})
+        stats = await self.db.find_one({"id": user.id})
 
         if stats is None:
-            return await ctx.send(f'User {user.name} hasn\'t sent a message.')
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description=f"User {user.name} hasn't sent a single message here.",
+                color=self.bot.main_color,
+            )
 
-        exp = stats['exp']
-        gold = stats['gold']
-        level = stats['level']
+            return await ctx.send(embed=embed)
 
-        await ctx.send(f'{user.name} is level {level} and has {exp} experience points. They also have {gold} gold.')
+        embed = discord.Embed(
+            title="Leveling",
+            url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+            description=f"{user.name} is level "
+            + str(stats["level"])
+            + " and has "
+            + str(stats["exp"])
+            + " experience points. They also have "
+            + str(stats["gold"])
+            + " gold.",
+            color=self.bot.main_color,
+        )
 
-    @level.command(name='amount')
+        await ctx.send(embed=embed)
+
+    @level.command(name="amount")
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def amount(self, ctx, amount: str = None):
+    async def amount(self, ctx, amount):
         """Change the amount of gold given to a user per message."""
 
         try:
             amount = int(amount)
-        except Exception:
-            return await ctx.send('That doesn\'t seem like a valid number.')
+        except ValueError:
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description="That doesn't look like a valid number.",
+                color=self.bot.main_color,
+            )
+
+            return await ctx.send(embed=embed)
 
         if amount < 1:
-            return await ctx.send('I can\'t give negative gold.')
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description="I can't give negative gold.",
+                color=self.bot.main_color,
+            )
 
-        config = await self.db.find_one({'_id': 'leveling-config'})
+            return await ctx.send(embed=embed)
+
+        config = await self.db.find_one({"_id": "leveling-config"})
 
         if config is None:
-            await self.db.insert_one({
-                '_id': 'leveling-config',
-                'amount_per_message': amount
-            })
-            await ctx.send(f'I successfully set the amount of gold given to {amount}.')
-        else:
-            await self.db.update_one({'id': 'leveling-config'}, {'$set': {'amount': amount}})
-            await ctx.send(f'I successfully updated the amount of gold given to {amount}.')
+            await self.db.insert_one(
+                {"_id": "leveling-config", "amount_per_message": amount}
+            )
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description=f"I set the amount of gold given to {amount}.",
+                color=self.bot.main_color,
+            )
 
-    @level.command(name='leaderboard', aliases=['lb'])
+            await ctx.send(embed=embed)
+        else:
+            await self.db.update_one(
+                {"id": "leveling-config"}, {"$set": {"amount": amount}}
+            )
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description=f"I updated the amount of gold given to {amount}.",
+                color=self.bot.main_color,
+            )
+
+            await ctx.send(embed=embed)
+
+    @level.command(name="leaderboard", aliases=["lb"])
     @checks.has_permissions(PermissionLevel.REGULAR)
     async def leaderboard(self, ctx):
-        """Check who has the most exp points."""
+        """Check who has the most experience points."""
 
-        users = self.db.find({}).sort('exp', -1)
+        users = self.db.find({}).sort("exp", -1)
 
         embed = discord.Embed(
-            title='Leaderboard for ' + ctx.guild.name,
-            colour=discord.Colour.blurple()
+            title="Leaderboard for " + ctx.guild.name, colour=discord.Colour.blurple(),
+            url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
         )
 
         for user in await users.to_list(length=11):
             try:
-                embed.add_field(name=user['name'], value=str(user['exp']) + ' exp')
+                embed.add_field(name=user["name"], value=str(user["exp"]) + " exp")
             except KeyError:
                 continue
 
         await ctx.send(embed=embed)
 
-    @level.command(name='give')
+    @level.command(name="give")
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def give(self, ctx, user: discord.User = None, amount: str = None):
+    async def give(self, ctx, user: discord.User, amount):
         """Give a specific amount of gold to a user."""
-
-        if user is None or amount is None:
-            return
 
         try:
             amount = int(amount)
-        except Exception:
-            return ctx.send('That doesn\'t seem like a valid number.')
+        except ValueError:
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description="That doesn't look like a valid number.",
+                color=self.bot.main_color,
+            )
+
+            return await ctx.send(embed=embed)
 
         if amount < 1:
-            return await ctx.send('I can\'t give negative gold.')
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description="I can't give negative gold.",
+                color=self.bot.main_color,
+            )
 
-        stats = await self.db.find_one({'id': user.id})
+            return await ctx.send(embed=embed)
+
+        stats = await self.db.find_one({"id": user.id})
 
         if stats is None:
-            return await ctx.send('That user hasn\'t sent a message here.')
-        else:
-            gold = int(stats['gold'])
+            embed = discord.Embed(
+                title="Leveling",
+                url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+                description=f"User {user.name} hasn't sent a single message here.",
+                color=self.bot.main_color,
+            )
 
-            await self.db.update_one({'id': user.id}, {'$set': {'gold': gold + amount}})
-            await ctx.send(f'I gave {amount} gold to {user.name}.')
+            return await ctx.send(embed=embed)
+
+        gold = int(stats["gold"])
+
+        await self.db.update_one({"id": user.id}, {"$set": {"gold": gold + amount}})
+
+        embed = discord.Embed(
+            title="Leveling",
+            url="https://github.com/papiersnipper/modmail-plugins/blob/master/leveling",
+            description=f"I gave {amount} gold to {user.name}",
+            color=self.bot.main_color,
+        )
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):

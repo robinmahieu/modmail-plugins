ff1625a5ee81a6361188b885219c97eae90cc4af
papiersnipper <robin.mahieu@protonmail.com>
2019-10-04 15:04:24 +0200

    Update autorole plugin

--- autorole/README.md
+++ autorole/README.md
@@ -5,14 +5,15 @@
 
 ## Installation
 
-To install this plugin, use this command in your Modmail server: `plugin add autorole`
+To install this plugin, use this command in your Modmail server: `plugin add autorole`.
 
 ## Commands
 
-| name          | usage              | example               | permission         | description                                       |
-|---------------|--------------------|-----------------------|--------------------|---------------------------------------------------|
-| autorole set  | autorole set role  | autorole set @Member  | Administrator [4]  | Sets the default role a member gets when joining. |
-| autorole give | autorole give role | autorole give @Member | Administrator [4]  | Gives this role to all members of your server.    |
+| name           | usage              | example               | permission        | description                                         |
+| -------------- | ------------------ | --------------------- | ----------------- | --------------------------------------------------- |
+| autorole clear | autorole clear     | autorole clear        | Administrator [4] | Clear the default role(s).                          |
+| autorole give  | autorole give role | autorole give @Member | Administrator [4] | Give the joined member all currently set roles.     |
+| autorole set   | autorole set role  | autorole set @Member  | Administrator [4] | Set the default role(s) a member gets when joining. |
 
 ## Permissions
 
--- autorole/autorole.py
+++ autorole/autorole.py
@@ -1,65 +1,96 @@
-import logging
+"""
+Autorole plugin for Modmail.
+
+Written by Papiernsipper.
+All rights reserved.
+"""
 
 import asyncio
-import discord
-from discord.ext import commands
+import logging
 
-from core import checks
+from discord import Embed, Guild, Member, Role
+from discord.ext.commands import Bot, Cog, Context, Greedy, group
+from discord.utils import get
+
+from core.checks import has_permissions
 from core.models import PermissionLevel
 
-Cog = getattr(commands, "Cog", object)
 
 logger = logging.getLogger("Modmail")
 
 
 class Autorole(Cog):
-    """Auto-assign a role to a user when they join your server.
+    """
+    Auto-assign a role to a user when they join your server.
     More info: [click here](https://github.com/papiersnipper/modmail-plugins/tree/master/autorole)
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot: Bot) -> None:
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
-        asyncio.create_task(self.api_post())
+        asyncio.create_task(self.migrate())
 
-    async def api_post(self):
+    async def migrate(self) -> None:
+        """Migrates the database model to contain a list of role ID's instead of the ``rolename`` field."""
+        config = await self.db.find_one({"_id": "autorole-config"})
 
-        async with self.bot.session.post(
-            "https://papiersnipper.herokuapp.com/modmail-plugins/autorole/"
-            + str(self.bot.user.id)
-        ):
-            pass
+        if config is None:
+            return
+
+        try:
+            rolename = config["rolename"]
+        except KeyError:
+            return
+
+        guild: Guild = get(self.bot.guilds, id=int(self.bot.config["guild_id"]))
+
+        if guild is None:
+            return await self.db.delete_one({"_id": "autorole-config"})
+
+        role: Role = get(guild.roles, name=rolename)
+
+        roles = []
+        roles.append(role.id)
+
+        await self.db.replace_one({"_id": "autorole-config"}, {"roles": roles})
 
     @Cog.listener()
-    async def on_member_join(self, member):
+    async def on_member_join(self, member: Member) -> None:
+        """Give the joined member all currently set roles."""
         config = await self.db.find_one({"_id": "autorole-config"})
 
         if config is None:
             return logger.warning("Member joined while no role was set!")
 
         try:
-            rolename = config["rolename"]
+            role_ids = config["roles"]
         except KeyError:
             return logger.error("Something went wrong in your database!")
 
-        role = discord.utils.get(member.guild.roles, name=rolename)
+        if not role_ids:
+            return
 
-        if role is None:
-            return logger.error(f"Unknown role {rolename}!")
+        roles = []
+        for role_id in role_ids:
+            role: Role = get(member.guild.roles, id=role_id)
 
-        await member.add_roles(role)
+            if role is not None:
+                roles.append(role)
 
-    @commands.group(name="autorole", invoke_without_command=True)
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def autorole(self, ctx):
+        await member.add_roles(*roles)
+
+    @group(name="autorole", invoke_without_command=True)
+    @has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def autorole(self, ctx: Context) -> None:
         """Auto-assign a role to a user when they join your server."""
-
         await ctx.send_help(ctx.command)
 
     @autorole.command(name="set")
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def set_(self, ctx, role: discord.Role):
-        """Sets the default role a member gets when joining."""
+    @has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def autorole_set(self, ctx: Context, roles: Greedy[Role]) -> None:
+        """Set the default role(s) a member gets when joining."""
+        if not roles:
+            return await ctx.send_help(ctx.command)
 
         config = await self.db.find_one({"_id": "autorole-config"})
 
@@ -67,24 +98,26 @@ class Autorole(Cog):
             await self.db.insert_one({"_id": "autorole-config"})
             logger.info("Created autorole config file.")
 
+        role_ids = [r.id for r in roles]
+        role_mentions = [r.mention for r in roles]
+
         await self.db.find_one_and_update(
-            {"_id": "autorole-config"}, {"$set": {"rolename": role.name}}
+            {"_id": "autorole-config"}, {"$set": {"roles": role_ids}}
         )
 
-        embed = discord.Embed(
+        embed = Embed(
             title="Autorole",
             url="https://github.com/papiersnipper/modmail-plugins/blob/master/autorole",
-            description=f"I will now give {role.mention} to all new members.",
-            colour=self.bot.main_color,
+            description=f"{', '.join(role_mentions)} will now be given to all new members.",
+            color=self.bot.main_color,
         )
 
         await ctx.send(embed=embed)
 
     @autorole.command(name="give")
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def give(self, ctx, role: discord.Role):
-        """Gives this role to all members of your server."""
-
+    @has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def autorole_give(self, ctx: Context, role: Role) -> None:
+        """Give this role to all members of your server."""
         users = 0
         for member in ctx.guild.members:
             if role.id in [role.id for role in member.roles]:
@@ -93,7 +126,7 @@ class Autorole(Cog):
                 await member.add_roles(role)
                 users = users + 1
 
-        embed = discord.Embed(
+        embed = Embed(
             title="Autorole",
             url="https://github.com/papiersnipper/modmail-plugins/blob/master/autorole",
             description=f"Added {role.mention} for {users} members.",
@@ -102,6 +135,29 @@ class Autorole(Cog):
 
         await ctx.send(embed=embed)
 
+    @autorole.command(name="clear", aliases=["reset"])
+    @has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def autorole_clear(self, ctx: Context) -> None:
+        """Clear the default role(s)."""
+        embed = Embed(
+            title="Autorole",
+            url="https://github.com/papiersnipper/modmail-plugins/blob/master/autorole",
+            description=f"Cleared role(s).",
+            color=self.bot.main_color,
+        )
 
-def setup(bot):
+        config = await self.db.find_one({"_id": "autorole-config"})
+
+        if config is None:
+            return await ctx.send(embed=embed)
+
+        await self.db.find_one_and_update(
+            {"_id": "autorole-config"}, {"$set": {"roles": []}}
+        )
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot: Bot) -> None:
+    """Bot cog load."""
     bot.add_cog(Autorole(bot))
